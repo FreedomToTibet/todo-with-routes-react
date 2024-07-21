64d0bdf82256102b7dedac6fb229f99a2a007b2c
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-07-21 01:58:04 -0700

    Add CORS headers to JSON server

--- api/server.js
+++ api/server.js
@@ -3,6 +3,14 @@ const server = jsonServer.create();
 const router = jsonServer.router('src/assets/db.json');
 const middlewares = jsonServer.defaults();
 
+// Add CORS headers
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 server.listen(3001, () => {
